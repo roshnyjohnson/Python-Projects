a1133761da676d615e2d4a226c9df26d87042110
ROSHNY JOHNSON <roshnyjn05@gmail.com>
2025-06-03 01:46:19 +0530

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,35 @@
+def add(num1,num2):
+    return num1+num2
+def sub(num1,num2):
+    return num1-num2
+def mul(num1,num2):
+    return num1*num2
+def div(num1,num2):
+    return num1/num2
+op_dict={
+    '+':add,
+    '-':sub,
+    '*':mul,
+    '/':div
+}
+def primary():
+
+    a=int(input("enter the first number"))
+    flag=True
+    while flag:
+        op = input("enter the operation\n+\n-\n*\n")
+        b=int(input("enter the next number"))
+
+        wanted=op_dict[op]
+        result=wanted(a,b)
+
+        print(result)
+        ans=input(f"enter y if you want to continue with {result} \nenter n if you want to start new \n enter  x if you want to exit")
+        if ans=="y":
+            a = result
+        elif ans=="n":
+            primary()
+        else:
+            exit(0)
+
+primary()
